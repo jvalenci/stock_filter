c85e030c333160c63b94d67dacd58ab0bdb61d7b
jonathanValencia <jonathan.valencia716@gmail.com>
2018-01-13 18:52:22 -0600

    Cleaned up test and rename test class

--- src/test/java/com/jvalenc/stock/csv/reader/CsvReaderTest.java
+++ src/test/java/com/jvalenc/stock/csv/reader/CsvReaderTest.java
@@ -9,20 +9,7 @@ import java.util.List;
 /**
  * Created by jonat on 1/13/2018.
  */
-public class CsvFileReaderTest {
-    @Test
-    public void readCsvFile() throws Exception {
-
-        //Arrange
-        String fileName = "C:\\Users\\jonat\\IdeaProjects\\stock-filter\\src\\main\\resources\\nyseList.csv";
-        CsvReader csvFileReader = new CsvReader();
-
-        //Act
-        List<StockTicker> stockTickers = csvFileReader.readCsvFile(fileName);
-
-        //Assert
-        Assert.assertTrue(stockTickers.size() > 0);
-    }
+public class CsvReaderTest {
 
     @Test
     public void ReadCsvDirectory() throws Exception {
